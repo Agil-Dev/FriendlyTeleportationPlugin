9e56983650a3b57a4a698b96c53014fb11de2ac6
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2021-06-14 21:03:04 +0200

    feat: implementation of CommandTeleportationAccept

--- src/main/java/com/friendlyteleportation/commandsHandlers/commands/CommandTeleportationAccept.java
+++ src/main/java/com/friendlyteleportation/commandsHandlers/commands/CommandTeleportationAccept.java
@@ -1,7 +1,11 @@
 package com.friendlyteleportation.commandsHandlers.commands;
 
-import com.friendlyteleportation.commandsHandlers.CommandData;
-import com.friendlyteleportation.commandsHandlers.CommandRunner;
+import com.friendlyteleportation.FriendlyTeleportation;
+import com.friendlyteleportation.commandsHandlers.*;
+import org.bukkit.entity.Player;
+
+import java.time.LocalTime;
+import java.util.UUID;
 
 public class CommandTeleportationAccept extends CommandRunner {
     public CommandTeleportationAccept(CommandData data) {
@@ -10,6 +14,30 @@ public class CommandTeleportationAccept extends CommandRunner {
 
     @Override
     public boolean run() {
-        return false;
+        if(!(data.sender instanceof Player))
+            return false;
+        Player p = (Player) data.sender;
+
+        UUID requesterUUID = FriendlyTeleportation.teleportationRequests.get(p.getUniqueId());
+        if (requesterUUID == null) {
+            p.sendMessage("still no requests");
+            return false;
+        }
+
+        LocalTime lt = FriendlyTeleportation.teleportationRequestsTime.get(p.getUniqueId());
+        if (lt != null)
+            if (LocalTime.now().getSecond() - lt.getSecond() < 15){
+                p.sendMessage("still no requests");
+                return false;
+            }
+
+        Player requester = p.getServer().getPlayer(requesterUUID);
+
+        requester.teleport(p);
+
+        FriendlyTeleportation.teleportationRequests.remove(p.getUniqueId());
+        FriendlyTeleportation.teleportationRequestsTime.remove(p.getUniqueId());
+
+        return true;
     }
 }
